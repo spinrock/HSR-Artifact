2daeec46b3f2b41fb36d1f9aebe2d013ee5988da
satoshi.fukai <chemistreal2@gmail.com>
2025-04-12 14:49:45 +0000

    新パーティ分のデータセット追加

--- src/app/entities/charactor/config/charactorData/2048_gyaraga.ts
+++ src/app/entities/charactor/config/charactorData/2048_gyaraga.ts
@@ -29,6 +29,19 @@ export const gyaraga: Charactor = {
         sphereOptions: ['HP%'],
         ropeOptions: ['撃破特効']
       }
+    },
+    {
+      id: '2048-3',
+      artifacts: {
+        artifactIds: ['003', '006', '019'],
+        bodyOptions: ['治癒量'],
+        footOptions: ['速度']
+      },
+      ornaments: {
+        ornamentIds: ['1010', '1020'],
+        sphereOptions: ['HP%'],
+        ropeOptions: ['EP回復効率', '撃破特効']
+      }
     }
   ]
 };
--- src/app/entities/charactor/config/charactorData/2065_shujinkoKioku.ts
+++ src/app/entities/charactor/config/charactorData/2065_shujinkoKioku.ts
@@ -16,6 +16,19 @@ export const shujinkoKioku: Charactor = {
         sphereOptions: ['HP%'],
         ropeOptions: ['EP回復効率']
       }
+    },
+    {
+      id: '2065-2',
+      artifacts: {
+        artifactIds: ['014', '021', '024'],
+        bodyOptions: ['会心ダメージ'],
+        footOptions: ['速度']
+      },
+      ornaments: {
+        ornamentIds: ['1010', '1019', '1020'],
+        sphereOptions: ['HP%'],
+        ropeOptions: ['EP回復効率']
+      }
     }
   ]
 };
